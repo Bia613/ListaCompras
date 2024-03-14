047d737384fca0e4c14e3d3dfc3fa4d03f3bd715
Usuario <Usuario@CPD03-M08>
2024-03-14 14:01:37 -0300

    Adicionar arquivos de projeto.

---
+++ MauiListaCompras/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiListaCompras
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
